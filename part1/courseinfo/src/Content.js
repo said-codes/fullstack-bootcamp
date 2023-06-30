b65c540be0a80834ac816cdea7fa8c0542f32e07
Said Valencia Castrillo <savcsaid@gmail.com>
2023-06-30 15:03:30 -0500

    courseinfo exercise 1.1 modified

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,7 +1,9 @@
-const Content = ({part, exercises})=>{
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3})=>{
     return(
         <>
-           <p>{part} {exercises}</p>
+           <p>{part1} {exercises1}</p>
+           <p>{part2} {exercises2}</p>
+           <p>{part3} {exercises3}</p>
         </>
     )
 }
