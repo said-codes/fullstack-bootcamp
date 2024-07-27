7df8a7b987aba7fd77cb9847b98870c5b8f2e17c
Said Valencia Castrillo <savcsaid@gmail.com>
2024-07-27 10:10:01 -0500

    1.10 unicafe step 5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,27 +6,41 @@ const Button = ({onClick, text})=>{
   )
 }
 
+
+//statisticline component
+const StatisticLine = (props) => {
+  return(
+    <div>
+      <p>{props.text} {props.value}</p>
+    </div>
+  )
+}
+
 const Statistics = (props) => {
   //conditional rendering with all clicks
   if(props.all === 0){
     return(
       <div>
+        <h2>Statistics</h2>
         <p>No feedback given</p>
       </div>
     )
   }
-  return (
+
+  return(
     <div>
-      <h1>Statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.all}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.positive}%</p>
+      <StatisticLine text="good" value ={props.good} />
+      <StatisticLine text="neutral" value ={props.neutral} />
+      <StatisticLine text="bad" value ={props.bad} />
+      <StatisticLine text="all" value ={props.all} />
+      <StatisticLine text="average" value ={props.average} />
+      <StatisticLine text="positive" value ={props.positive + " %"} />
     </div>
   )
+
 }
+
+
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
