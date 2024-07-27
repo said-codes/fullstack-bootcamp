2abcbfda829ec8800a4a6efb28cf7945de421076
Said Valencia Castrillo <savcsaid@gmail.com>
2024-07-27 10:21:54 -0500

    1.11 unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,13 +6,15 @@ const Button = ({onClick, text})=>{
   )
 }
 
-
 //statisticline component
 const StatisticLine = (props) => {
   return(
-    <div>
-      <p>{props.text} {props.value}</p>
-    </div>
+    <>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+    </>
   )
 }
 
@@ -29,18 +31,21 @@ const Statistics = (props) => {
 
   return(
     <div>
+      <h2>Statistics</h2>
+      <table>
+      <tbody>
       <StatisticLine text="good" value ={props.good} />
       <StatisticLine text="neutral" value ={props.neutral} />
       <StatisticLine text="bad" value ={props.bad} />
       <StatisticLine text="all" value ={props.all} />
       <StatisticLine text="average" value ={props.average} />
       <StatisticLine text="positive" value ={props.positive + " %"} />
+      </tbody>
+      </table>
     </div>
   )
 
 }
-
-
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
